92f8984c17a136a550e0058db013ba6931f6297a
RBCeva <roman.bambura@cevalogistics.com>
2026-09-23 13:20:16 +0200

    Add update and download URLs in SIOC.user.js
    Added update and download URLs for the script.

--- SIOC.user.js
+++ SIOC.user.js
@@ -5,6 +5,8 @@
 // @description  Evaluates SPTC shipments for Cage, SIOC, and Oversize conditions based on QIFCR live dimensions.
 // @match        https://sptc.eu.aftx.amazonoperations.app/*
 // @match        https://sptc.eu.aft.amazonoperations.app/*
+// @updateURL    https://github.com/RBCeva/TM/raw/refs/heads/main/CSX.user.js
+// @downloadURL  https://github.com/RBCeva/TM/raw/refs/heads/main/CSX.user.js
 // @connect      qifcr.eu.aftx.amazonoperations.app
 // @grant        GM_xmlhttpRequest
 // @run-at       document-end
